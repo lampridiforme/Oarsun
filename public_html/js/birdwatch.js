d488a70d1a02615ebeb30e4b0d7523eb9936a50b
christinedai <christinedai@christines-air>
2017-08-01 21:12:43 -0700

    Basic css and js are almost done. Next up is adding more true content. Images are from a quick google search and will be removed once I make the true assets. Also using a 3rd part script for tabbed-out timer functionality.

--- public_html/js/birdwatch.js
+++ public_html/js/birdwatch.js
@@ -4,24 +4,36 @@
  * and open the template in the editor.
  */
 
+// total number of units I have currently
 var runningTotal = 0;
+// total number of unique elements seen
+var uniqueRunningTotal = 0;
+// increment for each click
 var clickmeUpcount = 1;
+// increment per second from the autotools
 var autoUpcount = 0;
-
+// how much item1 costs currently
 var item1cost = 10;
+// rate of increase by item1
+var item1rate = .5;
 
 // placeholder for dynamic upcounting
 function displayTotal() {
-    $(".totalcount").html(runningTotal);
+    $(".totalcount").text("About " + Math.round(runningTotal) + " birds attracted (" + uniqueRunningTotal + " unique)");
 }
 
 function displayRate() {
-    $(".rate").html(autoUpcount + " birds seen per second.");
+    if(autoUpcount <= 1) {
+        $(".rate").text("Attracting about " + Math.round(autoUpcount) + " bird per second.");
+    }
+    else {
+        $(".rate").text("Attracting " + autoUpcount + " birds per second.");   
+    }
 }
 
-function displayNewCost(item) {
+function displayNewCostItem1() {
     // TODO: figure out how to save original text and then use it
-    //$(item).html()
+    $(".shopitem1price").text("Costs " + Math.round(item1cost));
 }
 
 function calcNewCost(originalcost) {
@@ -29,7 +41,25 @@ function calcNewCost(originalcost) {
     return newcost;
 }
 
+function autoUpdate() {
+    runningTotal = runningTotal+(autoUpcount/100);
+    displayTotal();
+}
+
+// seconds elapsed since page load
+/*
+var time = 0;
+function debugTimer() {
+    time++;
+    $(".shoptitle").text(time);
+}
+*/
+
+// for our intents and purposes, the main function
 $(document).ready(function() {
+    
+    setInterval(autoUpdate, 10); // TODO: 3rd party script allows tab out to be accurate
+    //setInterval(debugTimer, 1000);
     
     // when the main clickable element is clicked
     $(".mainclick").click(function() {
@@ -38,14 +68,16 @@ $(document).ready(function() {
     });
     
     // when shop item is clicked
-    $(".shopitem1title").click(function() {
+    $(".shopitem1").click(function() {
         if(runningTotal-item1cost >= 0) {
             runningTotal -= item1cost;
-            var newCost = calcNewCost(item1cost);
-            // display here
-            //debug
-            $(".shopitem1desc").html("new price is " + item1cost);
+            item1cost = calcNewCost(item1cost);
+            autoUpcount += item1rate;
+            displayRate();
+            displayTotal();
+            displayNewCostItem1();
         }
     });
+    
 });
 
